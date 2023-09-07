46525dbde5349d103e457d56dfae8504e50aea10
true_mogician <truemogician@gmail.com>
2023-09-08 02:04:42 +0800

    chore: add LICENSE generator

---
+++ script/generate-license.ts
@@ -0,0 +1,12 @@
+import FileSystem from "fs";
+import Path from "path";
+import packageInfo from "../package.json";
+
+const licenseTemplate = FileSystem
+	.readFileSync(Path.join(__dirname, "../LICENSE.template"))
+	.toString("utf-8");
+const license = licenseTemplate
+	.replace("{{current_year}}", new Date().getFullYear().toString())
+	.replace("{{author}}", packageInfo.author.name)
+	.replace("{{email}}", packageInfo.author.email);
+FileSystem.writeFileSync(Path.join(__dirname, "../LICENSE"), license, "utf-8");
