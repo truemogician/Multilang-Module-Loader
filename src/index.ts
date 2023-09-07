58e347c43ff82d31021374bf97641f7e89b8468e
true_mogician <truemogician@gmail.com>
2023-09-08 00:30:29 +0800

    refactor: remove webpack and build with tsc

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,14 @@ import AsyncFs from "fs/promises";
 import Path from "path";
 import { pathToFileURL } from "url";
 
-declare var __non_webpack_require__: ((id: string) => any) | undefined;
-const dynamicRequire = typeof __non_webpack_require__ === "function" ? __non_webpack_require__ : require;
-
-async function tryRequireThenImport(module: string): Promise<any> {
+async function tryRequireThenImport<T = any>(module: string): Promise<T> {
 	let result: any;
 	try {
-		result = dynamicRequire(module);
+		result = require(module);
 	} catch (error: any) {
-		let importEsm: ((module: string) => Promise<{ default: any }>) | undefined;
-		try {
-			importEsm = new Function("id", "return import(id);") as any;
-		} catch (e) {
-			importEsm = undefined;
-		}
-		if (error.code === "ERR_REQUIRE_ESM" && importEsm) {
-			const urlForConfig = pathToFileURL(module).href;
-			result = (await importEsm(urlForConfig)).default;
-			return result;
-		}
-		throw error;
+		if (error?.code !== "ERR_REQUIRE_ESM")
+			throw error;
+		result = await import(pathToFileURL(module).href);
 	}
 	// For babel/typescript
 	if (result && typeof result === "object" && "default" in result)
@@ -48,5 +36,5 @@ export default async function loadModule<T = any>(path: string): Promise<T> {
 	if (!Object.keys(interpret.jsVariants).includes(ext))
 		throw new Error(`Unsupported file type: ${ext}`);
 	rechoir.prepare(interpret.jsVariants, path);
-	return await tryRequireThenImport(path);
+	return await tryRequireThenImport<T>(path);
 }
